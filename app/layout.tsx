c7333cc038adeb20eec470f540e75f7b383e8a88
Arturo Garcia <aega2399@outlook.com>
2026-04-20 20:40:55 -0600

    add: formulario de login y registro (sin funcionalidad)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Montserrat } from "next/font/google"
+import type { Metadata } from "next"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
-
-const fontMono = Geist_Mono({
+const montserrat = Montserrat({
   subsets: ["latin"],
-  variable: "--font-mono",
+  variable: "--font-montserrat",
 })
+
+export const metadata: Metadata = {
+  title: "Edufy - Plataforma de Aprendizaje en Línea",
+  description:
+    "Edufy es una plataforma de aprendizaje en línea que ofrece cursos interactivos y personalizados para estudiantes de todas las edades. Nuestra misión es hacer que la educación sea accesible, atractiva y efectiva para todos.",
+}
 
 export default function RootLayout({
   children,
@@ -18,9 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="es"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn("antialiased", montserrat.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
